ad12ef80a373dbfdbaddce4d539899fc3bf416cc
volafy <92111292+volafy@users.noreply.github.com>
2023-10-30 07:17:39 +0100

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,41 @@
-class Wektor2D
-{
+class Wektor2D{
+
     // Tutaj napisz implementacje klasy Wektor2D
+    private:
+        double X;
+        double Y;
+
+        Wektor2D(): X{0},Y{0}{}
+        
+
+        Wektor2D(double x, double y){
+            X = x;
+            Y = y;
+        }
+
+    public:
+
+        void setX(double x){X=x};
+        void setY(double y){Y=y};
+        double getX(){return X};
+        double getY(){return Y};
+
+        
+
 };
+Wektor2D operator+(Wektor2D v1, Wektor2D v2){
+
+    Wektor2D newvector;
+    nevwector.setX(v1.getX()+v2.getX());
+    newvector.setY(v1.getY()+v2.getY());
+
+    return (newvector);
+}
+
+double operator*(Wektor2D v1, Wektor2D v2){
+
+    result = v1.getX()*v2.getX()+v1.getY()*v2.getY();
+    
+
+    return (result);
+}
